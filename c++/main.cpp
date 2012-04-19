8d698f30ab6ca5b45af6d26644352ff7fe4dd8bf
Dae-seon Moon <daeseonmoon@gmail.com>
2012-04-19 11:59:36 -0400

    updated

--- c++/main.cpp
+++ c++/main.cpp
@@ -5,13 +5,13 @@
 
 using namespace std;
 
-int isContainThreeSixNine(int number);
+int hasThreeSixNine(int number);
 
 int main(int argc, char** argv){
     string strNum;
     int match = 0;
     for(int i = 1; i <= 100; i++){
-        match = isContainThreeSixNine(i);
+        match = hasThreeSixNine(i);
         if(match){
             for(int x = 0; x < match; x++){
                 cout << "clap ";
@@ -23,18 +23,20 @@ int main(int argc, char** argv){
     }
 }
 
-int isContainThreeSixNine(int number){
+int hasThreeSixNine(int number){
     int retVal = 0;
     int num = 0;
     string temp = "";
+    char charNum;
     if(number == 0) return 0;
     while(number > 0){
         temp+=number%10+48;
         number/=10;
     }
     for(int i = 0; i < temp.length(); i++){
-        num = atoi(&temp[temp.length()-i-1]);
-        if(!(num%3)) retVal++;
+        charNum = temp[temp.length()-i-1];
+        num = atoi(&charNum);
+        if(num != 0 && !(num%3)) retVal++;
     }
     return retVal;
 }
